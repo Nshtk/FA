b786016e82c2cd74ae9f5a9d02f2bab35ad6b861
Nshtk <slava2034@gmail.com>
2021-07-06 13:40:03 +0300

    Changes to Task 29.06.21
    Code refactoring, algorythm updates.

--- FA Task 29.06.21/main.cpp
+++ FA Task 29.06.21/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <cstdint>
+#include <vector>
 
 using namespace std;
 
+bool zeroCheck(uint16_t divider)
+{
+    if(divider==0)
+    {
+        cerr << "\nDivision by zero spotted, aborting calculation\n";
+        return true;
+    }
+    return false;
+}
 bool oddCheck(uint16_t elem)
 {
-    uint8_t count = 0;
+    uint8_t count=0;
 
-    for(int i=15; i>-1; i--)
+    for(int i=sizeof(elem)*8-1; i>-1; i--)
         if((elem >> i) & 1)
             count++;
 
@@ -15,15 +25,15 @@ bool oddCheck(uint16_t elem)
 }
 uint8_t getDegree(uint16_t elem)
 {
-    for(int i=15; i>-1; i--)
+    for(int i=sizeof(elem)*8-1; i>-1; i--)
         if((elem >> i) & 1)
             return i;
 }
-uint16_t mod(uint16_t res, uint8_t divider)                 
+uint16_t mod(uint16_t res, uint8_t divider, uint8_t deg_mod)
 {
-    uint8_t deg_mod = getDegree(divider);
-
-    for(int i=15; i>0; i--)
+    if(zeroCheck(divider))
+        return 0;
+    for(int i=sizeof(res)*8-1; i>0; i--)
     {
         if(deg_mod<i)
             res ^= (divider << (i - deg_mod)) ^ (((1 << i) | 0));
@@ -35,40 +45,51 @@ uint16_t mod(uint16_t res, uint8_t divider)
     return res;
 }
 
-class GaluaField
+void printPolynomial(uint16_t elem)
+{
+    for(int i=sizeof(elem)*8-1; i>-1; i--)
+        if((elem >> i) & 1)
+            printf("x^%d ", i);
+    cout << '\n';
+}
+void printPolynomialVector(const vector<uint8_t> &irreducibles)
+{
+    for(uint8_t it : irreducibles)
+    {
+        cout << "x^8 ";
+        printPolynomial(it);
+    }
+}
+
+class GaloisField
 {
 public:
-    uint8_t elements[256];                                  // В массиве хранятся все элементы поля
-
-    GaluaField()
-    {
-        for(int i=0; i<256; i++)
-            elements[i]=i;
-    }
     uint8_t add(uint8_t elem_a, uint8_t elem_b)
     {
         return elem_a^elem_b;
     }
-    uint8_t multiply(uint8_t elem_a, uint8_t elem_b, const uint16_t modulo)
+    uint8_t multiply(uint8_t elem_a, uint8_t elem_b, uint16_t modulo)
     {
+        if(zeroCheck(modulo))
+            return 0;
         uint8_t deg_initial_res, deg_mod, res=0;
         uint16_t initial_res = 0;
 
         for(int i=7; i>0; i--)
             if((elem_b >> i) & 1)
                 initial_res^=elem_a << i;
-        deg_initial_res=getDegree(initial_res); deg_mod=getDegree(modulo);
 
+        deg_initial_res=getDegree(initial_res); deg_mod=getDegree(modulo);
         if(deg_initial_res<deg_mod)
             return initial_res;
-        else                                                // Выше вынес алгоритм взятия модуля в отдельную функцию mod()
+        else
         {
             for(int i=15; i>0; i--)
             {
                 if((initial_res >> i) & 1)
                 {
                     if(deg_mod<i)
-                        res ^= (modulo << (i - deg_mod)) ^ (((1 << i) | 0));
+                        res^=(modulo << (i - deg_mod)) ^ (((1 << i) | 0));
                     else if(deg_mod==i)
                         res^=modulo^((1 << i) | 0);
                     else
@@ -78,59 +99,57 @@ public:
             return res;
         }
     }
-    uint8_t inverse(uint8_t elem, const uint16_t modulo)
+    uint8_t inverse(uint8_t elem, uint16_t modulo)
     {
-        uint16_t res=1;
+        uint8_t res=1;
+
         for(int i=7; i>-1; i--)
         {
             if((elem >> i) & 1)
                 res=multiply(res, elem, modulo);
-            else
-                res<<=1;
+            res=multiply(res, res, modulo);
         }
         return res;
     }
-    void findIrredusables()                                  // Функция выводит 60 полиномов вместо 30
+    vector<uint8_t> findIrreducibles()
     {
-        uint8_t divider, power;
+        vector<uint8_t> irreducibles;
+        uint8_t divider=0, power, deg_mod;
 
         for(uint16_t poly=0b111111111; poly>254; poly--)
         {
-            if(!(poly & 1))                                  // Полиномы без x^0 сразу пропускаются
+            if(!(poly & 1))
                 continue;
+
             power=getDegree(poly)/2;
             for(divider|=1 << power; divider<255; divider++)
             {
-                if(!mod(poly, divider))
+                deg_mod=getDegree(divider);
+                if(!mod(poly, divider, deg_mod))
                     break;
-                if(getDegree(divider)==power)                // Если не удалось поделить полином на все полиномы от степени 8/2 до степени 7, то полином проверяется на нечетность
+                if(deg_mod==power)
                 {
                     if(oddCheck(poly))
-                        print(poly);
+                        irreducibles.push_back(poly);
                     break;
                 }
             }
         }
-    }
-    void print(const uint16_t elem)
-    {
-        printf("%d: ", elem);
-        for(int i=15; i>-1; i--)
-            if((elem >> i) & 1)
-                printf("x^%d ", i);
-        cout << '\n';
+        return irreducibles;
     }
 };
 
 int main()
 {
-    GaluaField fld;
+    GaloisField fld;
     const uint16_t prime_poly=0b100011011;
 
-    //fld.print(fld.add(fld.elements[67], fld.elements[0]));
-    fld.print(fld.multiply(fld.elements[7], fld.elements[150], prime_poly));
-    //fld.print(fld.inverse(fld.elements[7], prime_poly));
-    //fld.findIrredusables();
+    printPolynomial(fld.add(67, 0));
+    printPolynomial(fld.multiply(21, 12, prime_poly));
+    printPolynomial(fld.inverse(140, prime_poly));
+
+    vector<uint8_t> irreducibles=fld.findIrreducibles();
+    printPolynomialVector(irreducibles);
 
     return 0;
 }
